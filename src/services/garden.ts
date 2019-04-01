fce51a6cb7ca1c082beaafc5b5db7ea0405c04e4
zal <541904577@qq.com>
2019-04-01 20:15:49 +0800

    ~

--- src/services/garden.ts
+++ src/services/garden.ts
@@ -1,7 +1,12 @@
 import request from '@/utils/request';
 import URL from '@/utils/url';
 
-export async function queryGardenList(params) {
+interface GardenLisyParams {
+  token: string;
+  garden_name?: string;
+}
+
+export async function queryGardenList(params: GardenLisyParams) {
   let url = URL.GARDEN_LIST
 
   return request(url, { paramStr: JSON.stringify(params)})
